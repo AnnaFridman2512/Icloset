240d09d584f075da57d1e8d369d6840b2160a50b
AdiWeinstein <adiatias@yahoo.com>
2021-08-01 00:22:34 +0300

    update Nav component

--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -1,14 +1,67 @@
 import './Nav.css';
-
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {useState} from 'react';
 
 export default function Nav() {
- return ( 
-    <nav></nav>                                                               
-  );                                                                        
-}                                                                           
+    const [showLinks, setShowlinks] = useState(false);
+    return ( 
+    <> 
+    <div className="header" >
+        <h2 className='logo'>
+            I CLOSET
+        </h2>
+        <nav className='Navbar' id={showLinks? 'hidden': ""}>
+            <ul>
+                <li>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-person"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
+                    <a>
+                        Log in
+                    </a>
+                </li>
+                <li>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-plus-square"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                    <a>
+                        Add Item</a>
+                </li>
+            </ul>
+        </nav>
+        <button className="menu-btn" onClick={() => setShowlinks(!showLinks)}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-list"
+                viewBox="0 0 16 16">
+                <path
+                    fill-rule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+        </button>
+        < div className='search'>
+            <input type='text' placeholder='Search...'></input>
 
-
-
-
-
+        </div >
+    </div>
+     </ >);
+}
