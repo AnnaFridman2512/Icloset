5f2b15b1f31ad076f1327b284e9ef9b24a025be9
AdiWeinstein <adiatias@yahoo.com>
2021-08-08 10:41:40 +0300

    home page + header

--- react-app/src/components/App/App.js
+++ react-app/src/components/App/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 
 import React from 'react';
-import Nav from '../Nav';
-import AddItem from '../AddItem';
-import Search from '../Search';
-import Liked from '../Liked';
-import Login from '../Login';
+import HomePage from '../HomePage';
 
 function App() {
   
     return(
       <>
-      <Nav />
-      <AddItem />
-      <Search />
-      <Liked />
-      <Login />
+     <HomePage/>
       </>
     );
 }
---
+++ react-app/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Nav from './Nav'
+import './Nav.css'
+
+function HomePage() {
+    return (
+        <div>
+           <Nav /> 
+           <img src='./image/home1500x500.jpg'></img>
+           <Footer />
+        </div>
+    )
+}
+
+export default HomePage
