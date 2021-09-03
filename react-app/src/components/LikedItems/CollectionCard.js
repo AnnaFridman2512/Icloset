65a71aac2cc0b6a17f06acd651cba6861633162e
AdiWeinstein <adiatias@yahoo.com>
2021-09-03 11:41:15 +0300

    add collection card component

--- react-app/src/components/LikedItems/CollectionCard.js
+++ react-app/src/components/LikedItems/CollectionCard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
-import './CollectionCard.css'
+import "./CollectionCard.css";
 
 function CollectionCard({ collection }) {
   const [likedcollection, setCollection] = useState([]);
@@ -22,26 +22,40 @@ function CollectionCard({ collection }) {
 
   return (
     <div>
-    <div className="collection-card">
-      <div className="Collection_Item"></div>
-      <DeleteOutlineOutlinedIcon className="delete_icon" onClick={deletebtn} />
+      <div className="collection-card">
+        <div className="Collection_Item"></div>
+        <DeleteOutlineOutlinedIcon
+          className="delete_icon"
+          onClick={deletebtn}
+        />
+      </div>
+
+      <div
+        className={
+          collection[0].productType !== "dress"
+            ? "main-component"
+            : "main-component-dress"
+        }
+      >
+        <img
+          className={collection[0].productType !== "dress" ? "first" : "dress"}
+          src={collection[0].imageUrl}
+          alt="top item "
+        />
+        {collection[0].productType !== "dress" ? (
+          <img
+            className="third"
+            src={collection[1].imageUrl}
+            alt="buttom item "
+          />
+        ) : null}
+        <img
+          className="second"
+          src={collection[2].imageUrl}
+          alt="shoes item "
+        />
+      </div>
     </div>
-                
-                <div className={collection[0].productType !== 'dress' ? 'main-component' : 'main-component-dress'}>
-
-                    <img className={collection[0].productType !== 'dress' ? "first" : 'dress'} src={collection[0].imageUrl} alt="top item " />
-                    {
-                        collection[0].productType !== 'dress' ?
-                            <img className="third" src={collection[1].imageUrl} alt="buttom item " />
-                            : null
-                    }
-                    <img className='second' src={collection[2].imageUrl} alt="shoes item " />
-              
-
-
-                </div>
-         
-</div>
   );
 }
 
