3e4fb61f6eb28e053ecaf1c69aa732b04cdd3e34
AdiWeinstein <adiatias@yahoo.com>
2021-09-17 15:20:41 +0300

    liked items carusel - working with json (data)

--- react-app/src/components/LikedItems/CollectionCard.js
+++ react-app/src/components/LikedItems/CollectionCard.js
@@ -1,30 +1,31 @@
 import React from "react";
+
 import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
 import "./CollectionCard.css";
 
-
-
-
-function CollectionCard({ collection, setCollection, id }) {
+function CollectionCard({ combination, setCollection, id , deleteBtn}) {
   
-    const deleteBtn = (id) => {
-      const newCollection = collection.filter(collection =>
-    collection.id !== id);   
-    setCollection(newCollection)
-  };
-
 
   return (
     <>
       <div className="collection-card">
-        <div className="Collection_Item"> 
-            {collection.map(({productType,imageUrl,type}) => <img key={type} className={`liked-${type}`} src={imageUrl} alt={productType} />)} 
+        <div className="Collection_Item">
+          {combination.map(({ productType, imageUrl, type }) => (
+            <img
+              key={type}
+              className={`liked-${type}`}
+              src={imageUrl}
+              alt={productType}
+            />
+          ))}
         </div>
-       
-        
+
         {/* {collection.length === 0 ? 'Loading...' : collection.map((item) => <Combination key={collection.id} {...item}/>)} */}
       </div>
-      <DeleteOutlineOutlinedIcon className="delete_icon" onClick={()=>deleteBtn(id)} />
+      <DeleteOutlineOutlinedIcon
+        className="delete_icon"
+        onClick={() => deleteBtn(id)}
+      />
     </>
   );
 }
--- react-app/src/components/LikedItems/LikedItems.js
+++ react-app/src/components/LikedItems/LikedItems.js
@@ -1,61 +1,89 @@
 import "./LikedItems.css";
+import Carousel from "react-bootstrap/Carousel";
 
 import React, { useState, useEffect } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
 import CollectionCard from "./CollectionCard";
 
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
+// todos = [{label: 'dasdas',id: 1, date: ''}]
 
 export default function LikedItems() {
+  const [collections, setCollections] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const [ collection, setCollection ] = useState([]);
-  const [id, setId] = useState(3)
-
+  const deleteBtn = (id) => {
+    setCollections(
+      collections.filter((collection) => {
+        return collection.id !== id;
+      })
+    );
+     if (collections.length -1 == currentIndex ){
+       setCurrentIndex(currentIndex - 1)
+     }
+  };
 
   // Get one item
-   useEffect (() => {
-    
-       fetch(`http://localhost:8080/likedItems/${id}`)
-       .then((response)=>response.json())
-       .then((data)=>{
-        console.log('collection data',data);
-        setCollection(data)
+  useEffect(() => {
+    fetch(`http://localhost:8080/likedItems/`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("collection data", data);
+        setCollections(data);
       });
-  },[id])
- 
-// useEffect(() => {
-//     setCollection(likedItems)
-// })
-console.log(collection)
+  }, []);
+
+  console.log("p", collections, currentIndex);
 
   return (
+    //     <div>
+    //     <Carousel >
+    //         <Carousel.Item>
+    //                 {collection.length > 0 && <CollectionCard collection={collection} setCollection={setCollection} key={id} />}
+    //         </Carousel.Item>
+    //     </Carousel>
+    // </div>
+
     <div className="liked-items">
-        <div className="carusel">
-            <div className="caruselInner">
+      <div className="carusel">
+        <div className="caruselInner">
+          <div className="left">
+            <ArrowBackIosIcon
+              onClick={() => {
+                setCurrentIndex(
+                  currentIndex - 1 < 0
+                    ? collections.length - 1
+                    : currentIndex - 1
+                );
+              }}
+            />
+          </div>
 
-                <div className="left" onClick={() => { id > 0 && setId(id-1)  }}>
-                   {/* currImg > 0 && setCurrImg(currImg - 1); */}
-               
-                    <ArrowBackIosIcon />
-                </div>
-                <div className="center">
+          <div className="center">
+            {collections.length > 0 && (
+              <CollectionCard
+                deleteBtn={deleteBtn}
+                id={collections[currentIndex].id}
+                combination={collections[currentIndex].combination}
+              />
+            )}
+          </div>
 
-
-                   {collection.length > 0 && <CollectionCard collection={collection} setCollection={setCollection} key={id} />}
-                   
-                
-                </div>
-                <div className="right" onClick={() => {id > collection.length && setId(id + 1)  
-                    // currImg < images.length - 1 && setCurrImg(currImg + 1);
-                }}>
-                    <ArrowForwardIosIcon />
-                </div>
-
-            </div>
+          <div className="right">
+            <ArrowForwardIosIcon
+              onClick={() => {
+                setCurrentIndex(
+                  currentIndex + 1 <= collections.length - 1
+                    ? currentIndex + 1
+                    : 0
+                );
+              }}
+            />
+          </div>
         </div>
-
-      
+      </div>
     </div>
   );
 }
