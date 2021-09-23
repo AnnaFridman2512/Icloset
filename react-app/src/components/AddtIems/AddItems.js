3ced3165a6eff8efa4f0b3d5f7d67bc782641a39
Anna-Fridman <annafridman81@gmail.com>
2021-09-23 20:29:13 +0300

    'clear' function added, all error mgs and logic added to AddItems component, need only to finish styling :))

--- react-app/src/components/AddtIems/AddItems.js
+++ react-app/src/components/AddtIems/AddItems.js
@@ -8,6 +8,8 @@ export default function AddItems(){
     const [fileTypeError, setFileTypeError] = useState(false);
     const [fileExistsError, setFileExistsError] = useState(false);
     const [itemAddedMsg, setItemAddedMsg] = useState(false);
+    const [selectExists, setSelectExists] = useState(true);
+    const [selectRED, setSelectRED] = useState(false);
     const [type, setType] = useState('');//top bottom shoes else 
     const [productType, setProductType] = useState('');//top pants shorts dress skirt shoes
     const [topSelect, setTopSelect] = useState(false);
@@ -20,6 +22,7 @@ export default function AddItems(){
     const handleTypechange = (e) => {
       setType(e.target.value);
       const type = e.target.value;
+      
 
       if(type === "top" ){
         setTopSelect(true);
@@ -48,11 +51,13 @@ export default function AddItems(){
       setProductType(e.target.value)
     }
 
-    const fileChangeHandler = (e) => {
+    const fileChangeHandler = (e) => {   
       setFileTypeError(false);//clear error message when new item added
       setFileExistsError(false);
       setItemAddedMsg(false);  
-      
+      setSelectExists(false);
+      setSelectRED(false);
+
       const selected = e.target.files[0];//The target property of the Event interface is a reference to the object onto which the event was dispatched.
                                           //.files[0] - Accessing the first selected file
       const allowedTypes =["image/png", "image/jpeg", "image/jpg"];
@@ -84,35 +89,58 @@ export default function AddItems(){
       }
       };
 
-     
-
+    
     const onSubmitHandler = (e) => {
         e.preventDefault();//prevent submit button default behavior
+        setSelectExists(true)
+        
          
     // Handle File Data from the state Before Sending
      const data = new FormData();//The FormData() constructor creates a new FormData object.
-     
+    
     data.append("file", fileData);//Adding a key/value pair to "data" using FormData.append:
     data.append("type", type);
     data.append("productType", productType);
 
 
+
+if(type !== "" && productType !== ""){
+
      fetch("http://localhost:8080/api/addItems", {
        method: "POST",
-       body: data,
-
+       body: data,   
    })
+  
      .then(res => {
-       if(res.status !== 201){
-         setFileExistsError(true)
+
+        if(res.status === 201){
+           setFileExistsError(false)
+           setItemAddedMsg(true)
+           setSelectRED(false)
+           setType("")
+           setProductType("")
         }else{
-          setFileExistsError(false)
-          setItemAddedMsg(true)
+           setFileExistsError(true)
+           setItemAddedMsg(false)
+           setSelectRED(false)
         }
-      })
-     };
+    })
+   }else{
+    setSelectRED(true)
+    setItemAddedMsg(false)
+   }
+  }
 
-
+    const clear = () =>{
+      setImgPreview(null)
+      setItemAddedMsg(false)
+      setFileTypeError(false)
+      setFileExistsError(false)
+      setSelectExists(true)
+      setSelectRED(false)
+      setType('');
+      setProductType('')
+    }
 
     return (
         
@@ -120,18 +148,22 @@ export default function AddItems(){
         <div className="container">
       <h1>Add items</h1>
       <form onSubmit={onSubmitHandler}>
+      {!selectExists && <p className="selectMsg">Please select type and product-type from dropdown menu</p>}
+      {selectRED && <p className="errorMsg">Please select type and product-type from dropdown menu</p>}
+      {imgPreview && (
+        <>
       <select value={type} onChange={handleTypechange} >
-                <option>-- select an option -- </option>
+                <option>Type</option>
                 <option>top</option>
                 <option>bottom</option>
                 <option>shoes</option>
                 <option>else</option>
             </select>                
-            <div>{type}</div>
 
 
+        
         <select value={productType} onChange={handleProductTypechange} >
-                <option>-- select an option -- </option>
+                <option>Product-type </option>
                 <option disabled={topSelect ? false : true}>top</option>
                 <option disabled={bottomSelect ? false : true} >pants</option>
                 <option disabled={bottomSelect ? false : true}>shorts</option>
@@ -139,12 +171,13 @@ export default function AddItems(){
                 <option disabled={elseSelect ? false : true}>dress</option>
                 <option disabled={shoesSelect ? false : true}>shoes</option>
             </select>                
-            <div>{productType}</div>
-            
+            </>
+      )}
         <div className="container2">
         {fileTypeError && <p className="errorMsg">File not supported</p>}
         {fileExistsError && <p className="errorMsg">Item already exists</p>}
         {itemAddedMsg && <p className="addedMsg">Item added to closet</p>}
+        
       <div
        className="imgPreview"
        style={{background: imgPreview ? `url("${imgPreview}") no-repeat center/contain `: "#131313"}} //if we choose am img- show preview, else show background color
@@ -160,7 +193,7 @@ export default function AddItems(){
       {/*if imgPreview exists we render the button*/}
       {imgPreview && (
         <>
-        <button onClick={()=> setImgPreview(null)}>Select different item</button>
+        <button onClick={clear}>Select different item</button>
         <button type="submit" >Add to closet</button>
         </>
       )}
