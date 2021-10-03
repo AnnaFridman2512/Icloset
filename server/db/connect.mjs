4d379a4bfeb814eeef7c599e76e259b936f944af
yehiel <yehiel9523@gmail.com>
2021-10-04 02:58:57 +0300

    add yehiel db uri

--- server/db/connect.mjs
+++ server/db/connect.mjs
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
 //connect to DB
-const dbURL = process.env.DB_URL || "mongodb://localhost:27017/Icloset";
+const dbURL = process.env.DB_URL_Y || process.env.DB_URL || "mongodb://localhost:27017/Icloset";
 
-export const connect = () =>{
-mongoose.connect(dbURL,{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(() => console.log('mongoDB is connected'))
-  .catch((err) => console.log(err));
+export const connect = () => {
+    mongoose.connect(dbURL, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        }).then(() => console.log('mongoDB is connected'))
+        .catch((err) => console.log(err));
 }
