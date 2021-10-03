523b8d09b8d71d3aa2d4ab318df307a506bb33d3
yehiel9523 <67610717+yehiel9523@users.noreply.github.com>
2021-10-03 15:38:36 +0300

    delete node modules server

--- server/db/Combination.model.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-
-
-const CombinationSchema = new mongoose.Schema({
-    combination: [{ type: Schema.Types.ObjectId, ref: 'SingleFile' }] //called populate() - reference documents in other collections (singlefile)
-});
-
-export const Combination = mongoose.model('Combination', CombinationSchema); 
