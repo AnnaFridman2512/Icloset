1cee91cb67d3f488721b4c0f2230feff817388ba
AdiWeinstein <adiatias@yahoo.com>
2021-10-08 11:58:03 +0300

    Adding conditions to the home page on a button Lets get started: If there are no items in the closet link to "add items" Otherwise link to 'main component"

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useContext } from "react";
 import "./HomePage";
 import BgImage from "../../Helpers/CarouselImage/homePage_bg.jpg";
 import "../HomePage/HomePage.css";
 // import MainComponent from "../MainComponent/MainComponent";
 import {Link} from "react-router-dom";
+import { ViewAllContext } from "../viewAll/ViewAllContext";
+
+
 
 
 function HomePage() {
+
+  const { items } = useContext(ViewAllContext);
+
   return (
     <div className="home" style={{ backgroundImage: `url(${BgImage})` }}>
       <h1 className="header">Nothing To Wear?</h1>
       {/* <div className="container">
         <MainComponent />
       </div> */}
-      <Link to="/mainComponent" className="getStartedBtn">Lets Get Started!</Link>
+      <Link to={ items.lengs > 0 ? '/addItems'  : '/mainComponent'} 
+      className="getStartedBtn">Lets Get Started!</Link>
     </div>
   );
 }
